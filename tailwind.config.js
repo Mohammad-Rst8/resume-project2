2315b1b06127fa739ecdf7eaaa92991016a5f7ef
Mohammad-Rst8 <mohammad.rostamimajd8@gmail.com>
2024-02-12 01:58:19 +0330

    ok bog

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,17 @@
 module.exports = {
   content: ['*.html','./scripts/app.js'],
   theme: {
-    fontFamily:{
-'v-bold':'v-bold',
-'v-medium':'v-medium',
-'v-regular':'v-regular',
-    },
     
     extend: {
+      fontFamily: {
+        vbold: 'vbold',
+        vmedium: 'vmedium',
+        vregular: 'vregular',
+      },
       backgroundImage:{
-        'back-one':"url('/images/pexels-rostislav-uzunov-5011647.jpg')"
+        'back-one':"url('https://s30.picofile.com/file/8472417526/pexels_rostislav_uzunov_5011647.jpg')"
       }
+      
     },
   },
   plugins: [],
